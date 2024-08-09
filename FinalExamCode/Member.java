c0f4c3b9541323f93048072e000b6619d6af3434
beksus <85835904+beksus@users.noreply.github.com>
2024-08-10 00:20:43 +0800

    nothing much

--- FinalExamCode/Member.java
+++ FinalExamCode/Member.java
@@ -4,4 +4,5 @@ public class Member extends User{
     public Member(String name, String email, String password, String contactNumber) {
         super(name, email, password, contactNumber);
     }
+
 }
